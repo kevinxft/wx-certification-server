0f68ffcdc3d9f6fda03511b1ea049f4cbdcd7958
kevinxft <kevinxft@gmail.com>
2020-04-12 15:44:18 +0800

    :construction: fix port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('wx')
-  app.enableCors() 
-  await app.listen(4444);
+  app.enableCors()
+  await app.listen(process.env.port || 4444)
 }
-bootstrap();
+bootstrap()
